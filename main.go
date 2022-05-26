89618df8b9b90934e2c0167fd3792bac503533a8
Traxiny <traxinyman@gmail.com>
2022-05-26 23:05:34 +0200

    failing with bug (restarted pod)

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*========================
 		Log-watcher
 	author: Patrik Jesko
-	last update: 23/05/22
+	last update: 26/05/22
 ==========================*/
 
 package main
@@ -13,10 +13,13 @@ import (
 	"log"
 	"bufio"
 	"os"
-	"regexp"
+	//"regexp"
 	"sync"
+	"strings"
+	"path/filepath"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	//"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -57,12 +60,12 @@ func GetPodStatus(podInterface kv1.PodInterface , podName string, ctx context.Co
 	
 	pod, err := podInterface.Get(ctx, podName, metav1.GetOptions{})
 	if err != nil {
-		return false
+		return true
 	}	
 	if pod.Status.Phase == v1.PodFailed || pod.Status.Phase == v1.PodSucceeded {
-	return false
+		return true
 	}
-	return true
+	return false
 }
 
 //Getting pod logs
@@ -71,9 +74,9 @@ func getPodLogs(pod *v1.Pod, podInterface kv1.PodInterface, ctx context.Context,
 	fmt.Printf("Now watching pod %s\n", pod.Name) //just testing
 	defer wg.Done()
 
-	file, err := os.Create("/tmp/"+pod.Name+".txt")
-	checkErr(err)
-	defer file.Close()
+	//file, err := os.Create("/tmp/"+pod.Name+".txt")
+	//checkErr(err)
+	//defer file.Close()
 	log.Printf("file named "+pod.Name+".txt created!") //testing
 	PodLogsConnection := podInterface.GetLogs(pod.Name, &v1.PodLogOptions{
 		Follow:    true,
@@ -85,41 +88,43 @@ func getPodLogs(pod *v1.Pod, podInterface kv1.PodInterface, ctx context.Context,
 	scanner := bufio.NewScanner(LogStream)
 	var line string	
 
-	for {//this needs a lot of improvement...maybe make a testing ground..almsot the same but doesnt have to run in cluster
+	for {//this needs a lot of improvement
 		for scanner.Scan() { //returns bool so if Scan is false (when scan stops)
-			/*select {
-			case <-cancelCtx.Done(): //dont know what to do here :/
-				fmt.Printf("What am I doing here?!\n")
-				break //I had return here maybe that was the problem (dont know when the context starts or whatever)
-			default:*/
 			line = scanner.Text()
-			_, err = file.WriteString(line)
+			//_, err = file.WriteString(line)
+			fmt.Printf(line)
 			checkErr(err)
-			//}
 		}
-		if GetPodStatus(podInterface, pod.Name, ctx) { //if false then I know there was an error or the pod succeeded/failed
+		//time.Sleep(100*time.Millisecond)
+		//wait.PollImmediateInfinite(time.Second, isPodRunning(podInterface, pod.Name, ctx))
+		if GetPodStatus(podInterface, pod.Name, ctx) { 
+			//true if deleted or finished(job)
 			return
 		}
-		/*
-		fmt.Printf("Out of Scan!\n") //toto bolo loopnute
-		if scanner.Err() != nil { //TODO : if pod doesnt exist end this goroutine
-			fmt.Printf("%s\n",scanner.Err())
-			//return probably
-		}*/
+
 	}
 }
 
 func main(){
+	/*
 	//creating in-cluster config
 	config, err := rest.InClusterConfig()
 	checkErr(err)
 	//creating clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	checkErr(err)
-	
-	ns, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace") //read the namespace name
+	*/
+	kubeconfig := filepath.Join(
+		os.Getenv("HOME"), ".kube", "config",
+    )
+    config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
     checkErr(err)
-
+	clientset, err := kubernetes.NewForConfig(config)
+	checkErr(err)
+	
+	ns := "jesko-ns"
+	//ns, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace") //read the namespace name
+    checkErr(err)
 	api := clientset.CoreV1()
 	podInterface := api.Pods(string(ns))
 	checkErr(err)
@@ -130,14 +135,14 @@ func main(){
     ch := watcher.ResultChan()
 	var wg sync.WaitGroup
 
-	r, _ := regexp.Compile(os.Getenv("FILTER"))
+	//r, _ := regexp.Compile(os.Getenv("FILTER"))
 
 	for event := range ch {
         pod, err := event.Object.(*v1.Pod)
         if !err{log.Fatal("udefined")} //fatal is risky..
 		switch event.Type {
 			case watch.Added:
-				if r.MatchString(pod.Name) {
+				if strings.Contains(pod.Name, "hello") {//r.MatchString(pod.Name) {
 					fmt.Printf("Pod named %s added!\n", pod.Name) //optional
 					wait.PollImmediateInfinite(time.Second, isPodRunning(podInterface, pod.Name, ctx))
 					wg.Add(1)
